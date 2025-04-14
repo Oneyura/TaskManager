4b3e9e3fc361cba38df3345586d57b3aa423319f
Oneyura <hryn.yuri@gmail.com>
2025-04-14 16:59:05 +0300

    fix template

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -16,9 +16,21 @@ from .views import (
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
-    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
-    path("tasks/<int:pk>/complete/", TaskCompleteView.as_view(), name="task-complete"),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
+    path(
+        "tasks/<int:pk>/complete/",
+        TaskCompleteView.as_view(),
+        name="task-complete"
+    ),
     path("tasks/<int:pk>/undo/", TaskUndoView.as_view(), name="task-undo"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
